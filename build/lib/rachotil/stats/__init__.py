d0a7739bd80ce9facd7c5c0cd12b566edf967acc
Chillosh <cervinkamilos08@gmail.com>
2026-04-05 23:12:20 +0200

    Trying to repair imports which doesnt work, idk if this works

---
+++ build/lib/rachotil/stats/__init__.py
@@ -0,0 +1,4 @@
+from .config import get_enabled_stats_blocks, load_stats_config, save_stats_config
+
+__all__ = ["get_enabled_stats_blocks", "load_stats_config", "save_stats_config"]
+
---
+++ build/lib/rachotil/ui/components/menu.py
@@ -0,0 +1,22 @@
+from textual.screen import ModalScreen
+from textual.widgets import OptionList
+from textual.widgets.option_list import Option
+
+
+class MenuScreen(ModalScreen[str]):
+    CSS_PATH = "../styles.tcss"
+
+    def compose(self):
+        yield OptionList(
+            Option("Stats", id="stats"),
+            Option("SSH", id="term"),
+            Option("Management", id="management"),
+            Option("Settings", id="settings"),
+            id="main_menu"
+        )
+
+    def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
+        self.dismiss(event.option_id)
+
+    def action_close(self) -> None:
+        self.dismiss(None)
