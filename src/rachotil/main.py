0f14c141a04956a1c66e774aed0acf73fcbb57e5
milos-cervinka <cervimi24@sps-prosek.cz>
2026-05-31 01:14:43 +0200

    Will have to refactor whole repo once again or idk, my smart refactorization for better looks just destroyed it

--- src/rachotil/main.py
+++ src/rachotil/main.py
@@ -6,3 +6,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#After some testing I found out nothing really works after whole day of refactoring so I will have to fix pretty much the whole thing, just because I wanted some better looking code
